9515a8c33f38fb7d612d409c368e72db1446adc0
Debora Ribeiro <debii.ribeiro@gmail.com>
2025-06-01 22:02:31 -0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,9 @@
+// Classe principal que inicia o programa
 public class Main {
     public static void main(String[] args) {
+        // Inicia a interface gráfica na thread correta
         javax.swing.SwingUtilities.invokeLater(() -> {
-            new AutorView().setVisible(true);
+            new AutorView().setVisible(true); // Abre a janela AutorView
         });
     }
 }
