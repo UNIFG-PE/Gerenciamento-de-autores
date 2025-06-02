74f271ed2a4c50710402bce667832d62e7ea36f1
Debora Ribeiro <debii.ribeiro@gmail.com>
2025-06-01 22:01:34 -0300

    Update Autor.java

--- src/Autor.java
+++ src/Autor.java
@@ -1,20 +1,27 @@
+// Classe que representa um autor
 public class Autor {
+    // Atributos do autor
     private int id;
     private String nome;
     private String informacoes;
 
+    // Getter e Setter do ID
     public int getId() {
         return id;
     }
     public void setId(int id) {
         this.id = id;
     }
+
+    // Getter e Setter do Nome
     public String getNome() {
         return nome;
     }
     public void setNome(String nome) {
         this.nome = nome;
     }
+
+    // Getter e Setter das Informações
     public String getInformacoes() {
         return informacoes;
     }
